0d26b3d6b111dd2dbf5e35127e40076b00f6d73e
Renato Borges <renatobfnunes@gmail.com>
2026-03-03 15:23:14 -0300

    feat: atualiza atividades
    1. Correção de atividades
    2. Novo resultado

--- #005 - Laços/#047 - Numeros Pares 1 a 20.py
+++ #005 - Laços/#047 - Numeros Pares 1 a 20.py
@@ -5,7 +5,7 @@ Use for e range() para imprimir os números de 1 a 20, apenas os pares.
 # number = 0
 
 for i in range(1, 21):
-    i % 2
-    print(f'{i}')
+    if i % 2 == 0:
+        print(f'{i}')
 
 # PRECISA DE CORREÇÃO
---
+++ #005 - Laços/#049 - Contagem While 1 a 10.py
@@ -0,0 +1,8 @@
+'''
+Use while para imprimir os números de 1 a 10.
+'''
+number = 0
+
+while number < 10:
+    number +=1
+    print(number)
