535fa978607f901fe046cf9c66e104df75c77977
Renato Borges <renatobfnunes@gmail.com>
2026-04-10 16:19:27 -0300

    feat: atualiza atividade
    1. Últimas atividades, fim de unidade.

--- #005 - Laços/#071 - Laços.py
+++ #005 - Laços/#071 - Laços.py
@@ -9,21 +9,49 @@ Crie um programa que:
 3. Mostre apenas os números ímpares
 4. Ignore números negativos (continue)
 '''
-numero = 0
+soma = 0
+contador = 0
 
 while True:
-    entrada = input('Digite os números para o cálulo e digite "SAIR" para finalizar: ')
-
-    if entrada.strip().lower() == 'sair':
+    entrada = input('Digite um número ou "sair": ').strip().lower()
+    
+    if entrada == 'sair':
         break
+    
+    try:
+        numero = int(entrada)
+    except:
+        print('Digite apenas números ou "sair"')
+        continue
+    
+    if numero < 0:
+        continue
+    
+    soma += numero
+    contador += 1
+    
+    if contador == 1:
+        maior = numero
+        menor = numero
+    else:
+        if numero > maior:
+            maior = numero
+        
+        if numero < menor:
+            menor = numero
+    
+    if numero % 2 != 0:
+        print(f'Ímpar: {numero}')
 
-    dados = int(entrada)
-    numero += dados
+if contador > 0:
+    media = soma / contador
 
-print(f'\nResultado: {numero}')
-
-contador = dados
-contador += 1
-
-print(f'Total de números digitados: {contador}')
+    print('\nRESULTADOS:')
+    print(f'Soma: {soma}')
+    print(f'Quantidade: {contador}')
+    print(f'Maior: {maior}')
+    print(f'Menor: {menor}')
+    print(f'Média: {media:.2f}')
+else:
+    print('Nenhum número válido foi digitado.')
 
