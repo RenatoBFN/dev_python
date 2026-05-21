6a41e91aedcd9f74dfc2b1fd5abffa608c5cfdc8
Renato Borges <renatobfnunes@gmail.com>
2026-05-21 17:12:13 -0300

    feat: atualiza atividade
    Atualização de dados de nova atividade

--- #002 - Operadores/#076 - Conversor de Tempo de Viagem.py
+++ #002 - Operadores/#076 - Conversor de Tempo de Viagem.py
@@ -9,3 +9,8 @@ Calcule:
 3. tempo da viagem em horas
 4. quantos minutos sobraram
 '''
+distancia = int(input('Qual a distância em KM? '))
+velocidade = int(input('Qual a média de velocidade? (KM/h) '))
+
+tempo_viagem = distancia / velocidade
+print(f'\nO tempo de viagem será de: {tempo_viagem:.0f}')
