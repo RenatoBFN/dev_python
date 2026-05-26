1a3fe3f769adf46528b683efcd137b059141fb2b
Renato Borges <renatobfnunes@gmail.com>
2026-05-26 20:33:38 -0300

    feat: atualiza atividades
    1. Novos teste de lógica

--- #002 - Operadores/#076 - Conversor de Tempo de Viagem.py
+++ #002 - Operadores/#076 - Conversor de Tempo de Viagem.py
@@ -9,8 +9,18 @@ Calcule:
 3. tempo da viagem em horas
 4. quantos minutos sobraram
 '''
-distancia = int(input('Qual a distância em KM? '))
-velocidade = int(input('Qual a média de velocidade? (KM/h) '))
+distancia = float(input('Qual a distância em KM? '))
+velocidade = float(input('Qual a média de velocidade? (KM/h) '))
 
-tempo_viagem = distancia / velocidade
-print(f'\nO tempo de viagem será de: {tempo_viagem:.0f}')
+tempo_total = distancia / velocidade
+
+horas = int(tempo_total)
+
+minutos = (tempo_total - horas) * 60
+
+print(f'\nDistância: {distancia:.0f}Km')
+print(f'Velocidade: {velocidade:.0f}Km/h')
+
+print(f'\nO tempo de viagem será de: {horas} horas e {minutos:.0f} minutos')
+
+
---
+++ #002 - Operadores/#077 - Sistema de Parcelamento Inteligente.py
@@ -0,0 +1,24 @@
+'''
+Crie um programa que:
+
+1. Pergunte:
+    valor do produto
+    quantidade de parcelas
+2. Calcule:
+    valor de cada parcela
+    resto que sobra da divisão
+'''
+valor_prod = float(input('Qual é o valor do produto? '))
+parcelas = int(input('Quantidade de parcelas: '))
+
+print(f'\nO produto X foi vendido por R${valor_prod:.0f} em {parcelas} vezes.')
+
+calc_parcela = valor_prod / parcelas
+valor_parcela = int(calc_parcela)
+valor_centavos = calc_parcela - valor_parcela * 60
+
+print(f'\nResumo da Venda:')
+print('Produto: X\n'
+      f'Valor.: R${valor_prod:.0f}\n'
+      f'Parcelamento: {parcelas}x\n'
+      f'Valor Final.: {parcelas}x de R${valor_parcela},{valor_centavos:.2f}')
