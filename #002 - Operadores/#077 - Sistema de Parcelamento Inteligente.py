9718c1c3fd42b883b3b3b732085eba00b23295bc
Renato Borges <renatobfnunes@gmail.com>
2026-05-28 20:45:05 -0300

    feat: atividades

--- #002 - Operadores/#077 - Sistema de Parcelamento Inteligente.py
+++ #002 - Operadores/#077 - Sistema de Parcelamento Inteligente.py
@@ -15,10 +15,11 @@ print(f'\nO produto X foi vendido por R${valor_prod:.0f} em {parcelas} vezes.')
 
 calc_parcela = valor_prod / parcelas
 valor_parcela = int(calc_parcela)
-valor_centavos = calc_parcela - valor_parcela * 60
+parte_decimal = calc_parcela - int(calc_parcela)
+centavos = parte_decimal * 100
 
 print(f'\nResumo da Venda:')
 print('Produto: X\n'
       f'Valor.: R${valor_prod:.0f}\n'
       f'Parcelamento: {parcelas}x\n'
-      f'Valor Final.: {parcelas}x de R${valor_parcela},{valor_centavos:.2f}')
+      f'Valor Final.: {parcelas}x de R${valor_parcela},{centavos:.0f}')
---
+++ #003 - Estruturas de Decisão/#078 - Sistema de Aprovação Escolar.py
@@ -0,0 +1,11 @@
+'''
+Crie um programa que:
+
+1. Peça a nota do aluno (0 a 10)
+2. Mostre:
+    Nota maior ou igual a 7 → Aprovado
+    Nota entre 5 e 6.9 → Recuperação
+    Nota menor que 5 → Reprovado
+    Se a nota for menor que 0 ou maior que 10: Nota inválida
+'''
+
