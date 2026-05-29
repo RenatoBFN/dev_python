6ab6a246ac32498be2f2ca9493e6a7ea74a06c28
Renato Borges <renatobfnunes@gmail.com>
2026-05-29 10:21:00 -0300

    feat: atualiza atividade
    1. Breve revisão

--- #003 - Estruturas de Decisão/#078 - Sistema de Aprovação Escolar.py
+++ #003 - Estruturas de Decisão/#078 - Sistema de Aprovação Escolar.py
@@ -8,4 +8,14 @@ Crie um programa que:
     Nota menor que 5 → Reprovado
     Se a nota for menor que 0 ou maior que 10: Nota inválida
 '''
+nome = input('Digite o primeiro nome do aluno: ')
+nota = float(input(f'Digite a nota do {nome}: '))
 
+if nota < 5:
+    print(f'{nome} está REPROVADO!')
+elif nota >= 5 or nota <= 6.9:
+    print(f'{nome} está de RECUPERAÇÃO!')
+elif nota >= 7:
+    print(f'{nome} está APROVADO!')
+else:
+    print('NOTA DIGITADA NÃO ATENDE AOS CRITÉRIOS DE AVALIATIVO!')
