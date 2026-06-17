4d384e59451b116cbe30f71972e008cf0fa896fc
Renato Borges <renatobfnunes@gmail.com>
2026-06-16 23:43:20 -0300

    feat: nova correção
    1. Correção de atividade
    2. Novas atividades

--- #005 - Laços/#094 - Somador com While.py
+++ #005 - Laços/#094 - Somador com While.py
@@ -5,12 +5,12 @@ Crie um programa que:
 3. Quando o usuário digitar: 0. O programa encerra.
 4. Mostre a soma de todos os números digitados.
 '''
-
 contador = 0
 
-while numero !=0:
+while True:
     numero = int(input('Digite um número inteiro: '))
-    print(contador)
-    contador +=1
+    if numero == 0:
+        break
+    contador += numero
 
-    TERMINAR <<<<
+print(contador)
