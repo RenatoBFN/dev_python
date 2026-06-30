60bbd756e39f211e3b9dccc71e95f44e5e18f8ce
Renato Borges <renatobfnunes@gmail.com>
2026-06-30 16:07:36 -0300

    feat: atualiza pasta

--- #007 - Fundamentos de Listas/#118 - Organizando a Lista.py
+++ #007 - Fundamentos de Listas/#118 - Organizando a Lista.py
@@ -5,3 +5,12 @@ lista original
 lista ordenada (sort())
 lista invertida (reverse())
 '''
+numero = [ ]
+
+while True:
+    dados = int(input('Digite números inteiros ou digite "0" para encerrar: '))
+    dados += numero
+    if dados == 0:
+        break
+
+print(numero)
